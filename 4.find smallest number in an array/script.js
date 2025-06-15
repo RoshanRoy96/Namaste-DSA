9c4d7e942c9e609594eb27b13352c577b3ae506d
Roshan Roy <roshanroy1121@gmail.com>
2025-06-15 13:19:46 +0530

    Added a note

--- 4.find smallest number in an array/script.js
+++ 4.find smallest number in an array/script.js
@@ -12,3 +12,5 @@ function findSmallest(arr) {
 
 const result = findSmallest(arr);
 console.log(result);
+
+// Note:- The highest possible integer is 'Infinity'. When we want to find smallest number in an array we need to set the smallest value to be the highest possible integer. Tht's why we set Infinity as the smallest value.
